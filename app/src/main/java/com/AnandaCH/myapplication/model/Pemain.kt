99f00dec6156065845e06b9874d3f66619d76965
anandaCH <83534718+anandaCH@users.noreply.github.com>
2022-06-23 19:35:30 +0700

    RecyclesView
    Tugas Pemograman mobile Recycles view
    ananda Chika hadi permana
    311910422

---
+++ app/src/main/java/com/AnandaCH/myapplication/model/Pemain.kt
@@ -0,0 +1,10 @@
+package com.AnandaCH.myapplication.model
+
+data class Pemain(
+    var nama : String,
+    var foto : Int,
+    var posisi : String,
+    var Tinggi : String,
+    var tempatlahir : String,
+    var tgllahir : String
+)
